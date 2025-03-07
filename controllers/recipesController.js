9672365cc1042a9c99f7f505d12d0f3ba175f180
Lamboulda <lambdaaur@gmail.com>
2025-03-07 14:45:46 +0100

    recipefinal1

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -1,9 +1,9 @@
-import recipes from "../models/recipes.js";
+import Recipes from "../models/recipes.js";
 
 export const getAllRecipes = async (req, res) => {
     try{
-        const Recipe = await recipes.find().populate('author', 'first_name last_name')
-        return res.status(200).json(Recipe)
+        const recipe = await Recipes.find().populate('author', 'first_name last_name')
+        return res.status(200).json(recipe)
         }
     catch(err){
         return res.status(400).json({message: 'Internal server error'})
@@ -15,7 +15,7 @@ export const createNewRecipe = async (req,res) => {
     const { title, category, country, description, ingredients, steps } = req.body
     const authorId = req.user.id
 
-    const newRecipe = new recipes({
+    const newRecipe = new Recipes({
         title,
         category,
         country,
@@ -27,7 +27,7 @@ export const createNewRecipe = async (req,res) => {
 
     try{
         await newRecipe.save()
-        return res.status(201).json({message: 'Recipe added',recipes: newRecipe})
+        return res.status(201).json({message: 'Recipe added',Recipes: newRecipe})
 
     }
     catch(err){
@@ -38,11 +38,11 @@ export const createNewRecipe = async (req,res) => {
 
 export const getRecipeByID = async (req,res) => {
     try{
-        const Recipe = await recipes.findById(req.params.id).populate('author');
-        if (!Recipe) {
+        const recipe = await Recipes.findById(req.params.id).populate('author');
+        if (!recipe) {
             return res.status(404).json({ message: 'Recipe not found' })
         }
-        res.status(200).json(Recipe)
+        res.status(200).json(recipe)
     }
     catch(err){
         return res.status(400).json({message: 'Internal server error'})
@@ -53,7 +53,7 @@ export const updateRecipe = async (req,res) => {
     const {title, category, country, description, ingredients, steps} = req.body
 
     try{
-        const updatedRecipe = await recipes.findByIdAndUpdate(
+        const updatedRecipe = await Recipes.findByIdAndUpdate(
             req.params.id, req.body, { new: true })
 
         if (!updatedRecipe) {
@@ -70,7 +70,7 @@ export const updateRecipe = async (req,res) => {
 
 export const deleteRecipe = async (req,res) => {
     try {
-        const deletedRecipe = await recipes.findByIdAndDelete(req.params.id);
+        const deletedRecipe = await Recipes.findByIdAndDelete(req.params.id);
         if (!deletedRecipe) {
           return res.status(404).json({ message: 'Recipe not found' });
         }
